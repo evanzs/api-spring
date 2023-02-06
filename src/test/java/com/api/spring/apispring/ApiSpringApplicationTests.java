d4bfbdf152b0fce15691afd556559bbec59589de
evanzs <evandrofidk@hotmail.com>
2023-02-06 18:05:56 -0300

    feat:(first comit)::sparkles:: initial commit

---
+++ src/test/java/com/api/spring/apispring/ApiSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.spring.apispring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
